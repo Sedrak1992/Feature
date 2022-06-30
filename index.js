e34abaeb4bcb9c3d5de6872e7ef6a269e0de751a
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-30 17:17:26 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const row = document.querySelector("#row");
+
+let i = localStorage.getItem("from");
+
+const arr = [
+  ` <h1>Feature  1</h1>
+<p>Lorem  consectetur adipisicing elit. Vitae impedit nihil sint
+ quidem voluptas neque sequi quasi quos eaque voluptatum! </p>`,
+  ` <h1>Feature  2</h1>
+ <p>Lorem  consectetur adipisicing elit. Vitae impedit nihil sint 
+ quidem voluptas neque sequi 
+ quasi quos eaque voluptatum! </p>`,
+  ` <h1>Feature  3</h1>
+ <p>Lorem  consectetur adipisicing elit. Vitae impedit nihil
+  sint quidem voluptas neque sequi quasi quos eaque voluptatum! </p>`,
+];
+row.innerHTML = `
+<div>
+        <ul>
+          <li><a href="#" onclick="tab(0)">Tab 1</a></li>
+          <li><a href="#"  onclick="tab(1)">Tab 2</a></li>
+          <li><a href="#"  onclick="tab(2)">Tab 3</a></li>
+        </ul>
+        <div id="content">
+         ${arr[i]}
+        </div>
+    </div>
+ `;
+const li = document.querySelectorAll("a");
+function tab(e) {
+  for (let i = 0; i < li.length; i++) {
+    li[i].classList.remove("da");
+  }
+  li[e].classList.add("da");
+  const content = document.querySelector("#content");
+  content.innerHTML = arr[e];
+  localStorage.setItem("from", e);
+}
+li[i].classList.add("da");
